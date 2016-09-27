8ffd70d81e7030c349d7a2728a3d5d64c81275a9
Valentin Shirokov <regellosigkeitsaxiom@yandex.ru>
2016-09-27 12:49:51 +0800

    Edited in workspaces

--- xmonad.hs
+++ xmonad.hs
@@ -73,59 +73,58 @@ myKeys =
     , ("M-x", spawn "pkill xmobar")
     , ("M-S-x", liftIO $ launchXmobars )
     
-    , ("M-e", windows $ W.greedyView "draw")
-    , ("M-S-e", windows $ W.shift "draw")
-    , ("M-r", windows $ W.greedyView "read")
-    , ("M-S-r", windows $ W.shift "read")
-    , ("M-w", windows $ W.greedyView "web")
-    , ("M-S-w", windows $ W.shift "web")
-    , ("M-q", windows $ W.greedyView "main")
-    , ("M-S-q", windows $ W.shift "main")
-    , ("M-a", windows $ W.greedyView "metr")
-    , ("M-S-a", windows $ W.shift "metr")
-    , ("M-s", windows $ W.greedyView "media")
-    , ("M-S-s", windows $ W.shift "media")
-    , ("M-d", windows $ W.greedyView "work")
-    , ("M-S-d", windows $ W.shift "work")
-    , ("M-f", windows $ W.greedyView "free")
-    , ("M-S-f", windows $ W.shift "free")
+    , ("M-e", windows $ W.greedyView "E")
+    , ("M-S-e", windows $ W.shift "E")
+    , ("M-r", windows $ W.greedyView "R")
+    , ("M-S-r", windows $ W.shift "R")
+    , ("M-w", windows $ W.greedyView "W")
+    , ("M-S-w", windows $ W.shift "W")
+    , ("M-q", windows $ W.greedyView "Q")
+    , ("M-S-q", windows $ W.shift "Q")
+    , ("M-a", windows $ W.greedyView "A")
+    , ("M-S-a", windows $ W.shift "A")
+    , ("M-s", windows $ W.greedyView "S")
+    , ("M-S-s", windows $ W.shift "S")
+    , ("M-d", windows $ W.greedyView "D")
+    , ("M-S-d", windows $ W.shift "D")
+    , ("M-f", windows $ W.greedyView "F")
+    , ("M-S-f", windows $ W.shift "F")
     , ("M-i", spawn "pkill xmobar; xmonad --recompile; xmonad --restart")
     , ("M-S-i", io (exitWith ExitSuccess))
-    , ("M-o", spawn "firefox")
-    , ("M-Print", spawn "scrot -s")
+    , ("M-o", spawn "firefox -P default")
+    --, ("M-Print", spawn "scrot -s")
     , ("M-v", sendMessage ToggleStruts)
     , ("M-\\", spawn "synclient TouchpadOff=1; xdotool mousemove 2000 2000")
-  --, ("M-S-l", spawn "synclient TouchpadOff=0; xdotool mousemove 683 384")
     , ("M-S-\\", spawn "synclient TouchpadOff=0")
     ]
 
 myLayoutHook = avoidStruts
              . smartBorders
-             . workSpaceHook2
-             . workSpaceHook
+             -- . workSpaceHook2
+             -- . workSpaceHook
              $ customLayoutHook
-workSpaceHook = onWorkspaces ["metr"] metrLayoutHook
-workSpaceHook2 = onWorkspaces ["media"] mediaLayoutHook
-mediaLayoutHook = Full ||| Tall 1 (5/100) (1/2)
-
-metrLayoutHook = tiled ||| Mirror tiled ||| Full
-    where tiled = Tall 2 (2/100) (1/2)
+--workSpaceHook = onWorkspaces ["metr"] metrLayoutHook
+--workSpaceHook2 = onWorkspaces ["media"] mediaLayoutHook
+--mediaLayoutHook = Full ||| Tall 1 (5/100) (1/2)
+--metrLayoutHook = tiled ||| Mirror tiled ||| Full
+    --where tiled = Tall 2 (2/100) (1/2)
 
 customLayoutHook = layoutHook defaultConfig
 
 myMH = myMH2 <+> manageHook defaultConfig
 
-myWorkspaces = ["main","web","draw","read","metr","media","free","work"]
+myWorkspaces = ["Q","W","E","R","A","S","D","F"]
+--myWorkspaces = ["main","web","draw","read","metr","media","free","work"]
 
 myMH2 = composeAll
-    [ className =? "mplayer2" --> doShift "media"
-    , className =? "MPlayer" --> doShift "media"
-    , className =? "Firefox" --> doShift "web"
-    , className =? "Evince" --> doShift "read"
-    , className =? "Inkscape" --> doShift "draw"
-    , className =? "Mcomix" --> doShift "read"
-    , className =? "Steam" --> doShift "free"
-    , className =? "adom" --> doShift "media"
-    , title =? "My experiment" --> doShift "media"
-    , stringProperty "WM_WINDOW_ROLE" =? "metr" --> doShift "metr"
+    [ className =? "mplayer2" --> doShift "F"
+    , className =? "MPlayer" --> doShift "F"
+    , className =? "Firefox" --> doShift "E"
+    , className =? "Evince" --> doShift "R"
+    , className =? "Inkscape" --> doShift "F"
+    , className =? "Mcomix" --> doShift "R"
+    , className =? "Steam" --> doShift "F"
+    , className =? "adom" --> doShift "F"
+    , title =? "My experiment" --> doShift "S"
+    -- , stringProperty "WM_WINDOW_ROLE" =? "metr" --> doShift "metr"
     ] --Добавить автоматическое убирание дока при посещении "media"
